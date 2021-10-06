fe29ee1d49cca75e95fea1d68005d03f86144126
adrian4448 <adrian.morais4448@gmail.com>
2021-10-06 16:33:55 -0300

    Conclusão capitulos relacionados a componentes

---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Contadores</h1>
+    <app-contador v-for="c in 5" :key="c" /> 
+  </div>
+  
+</template>
+
+<script>
+import Contador from './Contador.vue'
+export default {
+    name: 'Contadores',
+    components: { 'app-contador': Contador }
+}
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid black;
+  }
+</style>
